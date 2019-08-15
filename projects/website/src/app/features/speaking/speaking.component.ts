f50fcf3b9bd07a7c92d1583cc7994831c4900910
tomastrajan <tomas.trajan@gmail.com>
2019-08-15 08:32:31 +0200

    feat(speaking): add content

--- projects/website/src/app/features/speaking/speaking.component.ts
+++ projects/website/src/app/features/speaking/speaking.component.ts
@@ -54,5 +54,47 @@ const TALKS = [
     slides: 'https://slides.com/tomastrajan/angular-schematics',
     event: 'https://cfpvdz19.confinabox.com/talk/DMF-3644',
     recording: 'https://www.youtube.com/watch?v=eFzjY4uWgCA'
+  },
+  {
+    title: 'Guide to custom Angular Schematics',
+    subtitle: 'Angular Zurich Meetup 03 2019 - Switzerland',
+    description:
+      'This presentation will be in form of live coding in which we explore how we can use Angular Schematics provided out of the box, how to create our own custom schematics and how to integrate them into Angular CLI workspaces.',
+    logo: 'angularzurich-logo.png',
+    logoAlt: 'Angular Zurich Meetup 03 2019',
+    photo: 'angularzurich201903.png',
+    photoAlt:
+      'Tomas Trajan speaking about Angular Schematics at Angular Zurich Meetup 03 2019',
+    slides: 'https://slides.com/tomastrajan/angular-schematics',
+    event: 'https://www.meetup.com/AngularZRH/events/258725129/',
+    recording: 'https://www.youtube.com/watch?v=M5YSPas3qFo'
+  },
+  {
+    title: 'Release Butler (Twitter Bot Sideproject)',
+    subtitle: 'Kuala Lumpur JS Meetup 2019 - Malaysia',
+    description:
+      'Release Butler - A twitter bot that helps you to stay up to date by with releases with popular frontend framework and libraries like Angular, React, Vue... by tweeting their changelogs!',
+    logo: 'kljs-logo.png',
+    logoAlt: 'Kuala Lumpur JS Meetup 2019',
+    photo: 'kljs2019.png',
+    photoAlt:
+      'Tomas Trajan speaking about Angular Schematics at Angular Zurich Meetup 03 2019',
+    slides: 'https://slides.com/tomastrajan/release-butler/',
+    event: 'https://www.meetup.com/Junior-Developers-Malaysia/events/262661606/',
+    recording: ''
+  },
+  {
+    title: 'How to build Angular Schematics',
+    subtitle: 'BernerJS Meetup 04 2019 - Switzerland',
+    description:
+      'This presentation will be in form of live coding in which we explore how we can use Angular Schematics provided out of the box, how to create our own custom schematics and how to integrate them into Angular CLI workspaces.',
+    logo: 'bernerjs-logo.png',
+    logoAlt: 'BernerJS Meetup 04 2019',
+    photo: 'bernerjs201904.png',
+    photoAlt:
+      'Tomas Trajan speaking about Angular Schematics at Angular Zurich Meetup 03 2019',
+    slides: 'https://slides.com/tomastrajan/angular-schematics',
+    event: 'https://www.meetup.com/Barner-JS-Talks/events/261475983/',
+    recording: ''
   }
 ];
