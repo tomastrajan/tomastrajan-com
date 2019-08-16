f6f96e8b43ca7a5370e298d55698bd41fcc2393e
tomastrajan <tomas.trajan@gmail.com>
2019-08-16 22:39:10 +0200

    feat(server): add github api handler

--- server.ts
+++ server.ts
@@ -1,21 +1,23 @@
 import 'zone.js/dist/zone-node';
+import * as express from 'express';
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+import { join } from 'path';
 import { enableProdMode } from '@angular/core';
-// Express Engine
 import { ngExpressEngine } from '@nguniversal/express-engine';
-// Import module map for lazy loading
 import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
-import * as express from 'express';
-import { join } from 'path';
-
-// Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
-
-// Express server
-const app = express();
+dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist/browser');
+const API_URL_GITHUB = 'https://api.github.com';
+const API_TOKEN_GITHUB = Buffer.from(
+  `tomastrajan:${process.env.API_TOKEN_GITHUB}`
+).toString('base64');
+
+const app = express();
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
 const {
@@ -36,7 +38,20 @@ app.set('view engine', 'html');
 app.set('views', DIST_FOLDER);
 
 // Example Express Rest API endpoints
-// app.get('/api/**', (req, res) => { });
+app.get('/api/github/repositories', async (req, res) => {
+  const options = { headers: { Authorization: `Basic ${API_TOKEN_GITHUB}` } };
+  Promise.all([
+    fetch(`${API_URL_GITHUB}/users/angular/repos?per_page=50`, options),
+    fetch(`${API_URL_GITHUB}/users/tomastrajan/repos?per_page=50`, options),
+    fetch(`${API_URL_GITHUB}/users/angular-extensions/repos`, options)
+  ])
+    .then(responses => Promise.all(responses.map(r => r.json())))
+    .then(responses =>
+      responses.reduce((result, next) => result.concat(next), [])
+    )
+    .then(repos => res.json(repos));
+});
+
 // Serve static files from /browser
 app.get(
   '*.*',
