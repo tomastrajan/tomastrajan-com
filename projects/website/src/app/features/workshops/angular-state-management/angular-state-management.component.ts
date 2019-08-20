55d0924a3d95af27a0bef59eeec5d62b06520d9a
tomastrajan <tomas.trajan@gmail.com>
2019-08-20 18:30:22 +0200

    feat(workshops): add content

--- projects/website/src/app/features/workshops/angular-state-management/angular-state-management.component.ts
+++ projects/website/src/app/features/workshops/angular-state-management/angular-state-management.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { ResponsiveLayoutService } from '../../../core/layout/responsive-layout.service';
 
 @Component({
   selector: 'tt-angular-state-management',
@@ -6,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./angular-state-management.component.scss']
 })
 export class AngularStateManagementComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+  isSmallOrSmaller: Observable<boolean>;
+
+  constructor(private responsiveLayoutService: ResponsiveLayoutService) {}
+
+  ngOnInit() {
+    this.isSmallOrSmaller = this.responsiveLayoutService.isSmallOrSmaller;
+  }
 }
