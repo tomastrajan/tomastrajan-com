a675250820c03a34426fd268dd0812b575f2f4cb
tomastrajan <tomas.trajan@gmail.com>
2019-08-23 09:31:21 +0200

    fix(app): scroll top on route change

--- projects/website/src/app/app-routing.module.ts
+++ projects/website/src/app/app-routing.module.ts
@@ -53,7 +53,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
