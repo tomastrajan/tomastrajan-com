2b9f2e6f70bc7bc1631a79a6dbbe91a58257f5d2
tomastrajan <tomas.trajan@gmail.com>
2019-08-23 09:50:07 +0200

    fix(styles): larger fonts on small devices, various responsive fixes

--- projects/website/src/app/app-routing.module.ts
+++ projects/website/src/app/app-routing.module.ts
@@ -53,7 +53,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
+  imports: [
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
