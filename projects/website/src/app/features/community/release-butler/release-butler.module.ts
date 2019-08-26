4ec3d855646ca1115f9ff81bb2c492805712f1ee
tomastrajan <tomas.trajan@gmail.com>
2019-08-26 13:47:10 +0200

    feat(community): release butler content

--- projects/website/src/app/features/community/release-butler/release-butler.module.ts
+++ projects/website/src/app/features/community/release-butler/release-butler.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../../../shared/shared.module';
+
 import { ReleaseButlerRoutingModule } from './release-butler-routing.module';
 import { ReleaseButlerComponent } from './release-butler.component';
 
 @NgModule({
   declarations: [ReleaseButlerComponent],
-  imports: [CommonModule, ReleaseButlerRoutingModule]
+  imports: [CommonModule, SharedModule, ReleaseButlerRoutingModule]
 })
 export class ReleaseButlerModule {}
