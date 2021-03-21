d01db8b951da022d19ab6e64a105b72bf076c117
tomastrajan <tomas.trajan@gmail.com>
2021-03-21 14:12:57 +0100

    feat(workshops): add anonymous feedback

--- projects/website/src/app/features/workshops/frontend-heroes/frontend-heroes.component.ts
+++ projects/website/src/app/features/workshops/frontend-heroes/frontend-heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GalleryImage } from '../../../shared/gallery/gallery.component';
+import { Testimonial } from '../../../shared/testimonials/testimonials.component';
 
 @Component({
   selector: 'tt-frontend-heroes',
@@ -8,11 +9,42 @@ import { GalleryImage } from '../../../shared/gallery/gallery.component';
 })
 export class FrontendHeroesComponent implements OnInit {
   images = IMAGES;
+  testimonials = TESTIMONIALS;
 
   constructor() {}
 
   ngOnInit(): void {}
 }
+
+const TESTIMONIALS: Testimonial[] = [
+  {
+    message:
+      'I enjoyed the course, and liked the presentation and exercises. I learned some angular good practices, which I could apply in my project!',
+    name: 'Workshop Participant',
+    role: 'Software Engineer (anonymous feedback)',
+    src: '../../../assets/images/testimonials/anonymous_grey_c.jpg',
+  },
+  {
+    message:
+      'After having used Angular without having had much web dev experience, I was struggling on understanding the black magic behind it all. Having all those bits and pieces together have helped me to grasp the philosophy of it. Thank you!',
+    name: 'Workshop Participant',
+    role: 'Software Engineer (anonymous feedback)',
+    src: '../../../assets/images/testimonials/anonymous_grey_d.jpg',
+  },
+  {
+    message: 'Both training agenda and hands-on work are well prepared!',
+    name: 'Workshop Participant',
+    role: 'Software Engineer (anonymous feedback)',
+    src: '../../../assets/images/testimonials/anonymous_grey_a.jpg',
+  },
+  {
+    message:
+      'Good workshop, basics are refreshed and concepts were presented in very clear manner!',
+    name: 'Workshop Participant',
+    role: 'Software Engineer (anonymous feedback)',
+    src: '../../../assets/images/testimonials/anonymous_grey_b.jpg',
+  },
+];
 
 const IMAGES: GalleryImage[] = [
   {
